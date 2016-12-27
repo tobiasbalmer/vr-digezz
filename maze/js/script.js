d958c23be0c5aaf5c8b6dc1667bdc961d2a21460
Tobias Balmer <tobias.balmer@mind.ch>
2016-12-27 21:24:39 +0100

    update trigger-others, better timing

--- maze/js/script.js
+++ maze/js/script.js
@@ -6,18 +6,27 @@ AFRAME.registerComponent('trigger-others', {
         trigger: {
             type: 'string',
             default: 'triggered' 
-        }       
+        },
+        repeatable: {
+            type: 'boolean',
+            default: false 
+        }        
     },
     init: function () {
                 
         var el = this.el,
             data = this.data,
-            blocks = data.target; 
+            blocks = data.target,
+            counter = 0; 
                 
-        el.addEventListener('mouseenter', function () {
-            for (var i = 0; i < blocks.length; i++) {
-                blocks[i].emit(data.trigger);
+        el.addEventListener('click', function () {
+            if (counter === 0) {
+                for (var i = 0; i < blocks.length; i++) {
+                    blocks[i].emit(data.trigger);
+                }  
+                if (!data.repeatable) counter++;
             }
+
         });
     }
 });
